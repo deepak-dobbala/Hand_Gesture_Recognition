efca60d30ab8b973c2b1e842b0adc6d36f2c2bc0
deepak-dobbala <deepakdobbala22@gmail.com>
2024-12-31 17:44:10 +0530

    Initial Code snippets

---
+++ src/hand_tracking.py
@@ -0,0 +1,37 @@
+import cv2
+import mediapipe as mp
+
+class HandTracker:
+    def __init__(self, static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+            static_image_mode=static_image_mode,
+            max_num_hands=max_num_hands,
+            min_detection_confidence=min_detection_confidence,
+            min_tracking_confidence=min_tracking_confidence
+        )
+        self.mp_draw = mp.solutions.drawing_utils
+
+    def find_hands(self, img, draw=True):
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(img_rgb)
+        
+        if self.results.multi_hand_landmarks:
+            for hand_landmarks in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mp_draw.draw_landmarks(img, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+        
+        return img
+
+    def find_position(self, img, hand_no=0, draw=True):
+        lm_list = []
+        if self.results.multi_hand_landmarks:
+            my_hand = self.results.multi_hand_landmarks[hand_no]
+            for id, lm in enumerate(my_hand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lm_list.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+        
+        return lm_list
---
+++ utils/helper.py
@@ -0,0 +1,11 @@
+import cv2
+
+def rescale_frame(frame, percent=75):
+    width = int(frame.shape[1] * percent / 100)
+
+    height = int(frame.shape[0] * percent / 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
+def draw_fps(img, fps):
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
